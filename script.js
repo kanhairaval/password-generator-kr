52d6720a6658610e1734dbaa41efa5929d959335
Kanhai Raval <kanhairaval@Kanhais-MacBook-Pro-2.local>
2022-11-06 18:18:31 -0500

    90% completion

--- script.js
+++ script.js
@@ -12,19 +12,47 @@ function buttonClick() {
     window.alert('Your password needs to be atleast 8 characters long, upto a maximum of 128 characters.');
     buttonClick();
   }
+  let finalString = ''
   let lowerInput = window.confirm('Do you want lower case characters in your password?');
+  if (lowerInput === true) {
+      finalString = finalString + lowerCaseAlphabet;
+      alert('Your password will have lower case characters.')
+  } else {
+      alert('Your password will not have lower case characters.')
+  }
   let upperInput = window.confirm('Do you want upper case characters in your password?');
+  if  (upperInput === true) {
+      finalString = finalString + upperCaseAlphabet;
+      alert('Your password will have upper case characters.')
+} else {
+      alert('Your password will not have upper case characters.')
+}
   let numberInput = window.confirm('Do you want numbers as characters in your password?');
+  if  (numberInput === true) {
+      finalString = finalString + numbers;
+      alert('Your password will have numbered characters.')
+} else {
+      alert('Your password will not have numbered characters.')
+}
   let symbolInput = window.confirm('Do you want symbols as characters in your password?');
-  };
+  if  (symbolInput === true) {
+      finalString = finalString + symbols;
+      alert('Your password will have symbol characters.')
+} else {
+      alert('Your password will not have symbol characters.')
+}
+return generateString(pwdInput, finalString);
+};
 
-  function generateString(length) {
-    let result = ' ';
-    const charactersLength = lowerCaseAlphabet.length;
+function generateString(pwdInput, finalString) {
+    let result = '';
+    const charactersLength = finalString.length;
     for ( let i = 0; i < length; i++ ) {
-        result += lowerCaseAlphabet.charAt(Math.floor(Math.random() * charactersLength));
+        result += finalString.charAt(Math.floor(Math.random() * charactersLength));
     }
-        return result;
+    console.log(result);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = result;
 }
 
 
@@ -33,13 +61,3 @@ function buttonClick() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
