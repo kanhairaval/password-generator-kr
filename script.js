0571eda4cb9851c0b55bc3e90a0be7f0894f1dc0
Kanhai Raval <kanhairaval@Kanhais-MacBook-Pro-2.local>
2022-11-06 01:06:07 -0400

    new update

--- script.js
+++ script.js
@@ -8,15 +8,13 @@ function buttonClick() {
   let pwdInput = window.prompt('How many characters between 8 & 128, would you like to have in your password?');
   if (pwdInput > 7 && pwdInput < 129) {
     let message = window.alert(`Your password will be ${pwdInput} characters long, please continue with the selection criteria.`);
-  } else {
+  } else if (pwdInput != pwdInput > 7 && pwdInput < 129) {
     window.alert('Your password needs to be atleast 8 characters long, upto a maximum of 128 characters.');
     buttonClick();
-  }
-};
-
-function userPrefLower () {
-  let lowerInput = window.prompt('Do you want lower case characters in your password?');
-};
+  } else {
+    window.prompt('Do you want lower case characters in your password?');
+    }
+  };
 
 
 // Get references to the #generate element
