562c75fc00583e78714f92a34ee7a070a1ba7e52
Kanhai Raval <kanhairaval@Kanhais-MacBook-Pro-2.local>
2022-11-05 23:43:36 -0400

    adding progress

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 // Assignment code here
+let lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+let upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let numbers = '0123456789';
+let symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+function buttonClick() {
+  let pwdInput = window.prompt('How many characters between 8 & 128, would you like to have in your password?');
+  if (pwdInput > 7 && pwdInput < 129) {
+    let message = window.alert(`Your password will be ${pwdInput} characters long, please continue with the selection criteria.`);
+  } else {
+    window.alert('Your password needs to be atleast 8 characters long, upto a maximum of 128 characters.');
+    buttonClick();
+  }
+};
+
+function userPrefLower () {
+  let lowerInput = window.prompt('Do you want lower case characters in your password?');
+};
 
 
 // Get references to the #generate element
